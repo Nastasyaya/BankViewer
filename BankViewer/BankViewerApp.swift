4f2d2ef17b43c38a5f130e43b91472a832b5fb62
Anastasiia <nastyak25555@gmail.com>
2025-08-06 20:00:25 +0200

    Initial Commit

---
+++ BankViewer/BankViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankViewerApp.swift
+//  BankViewer
+//
+//  Created by Анастасия Кутняхова on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BankViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankViewer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BankViewer
+//
+//  Created by Анастасия Кутняхова on 06.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
